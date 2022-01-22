042e4854761e15547389a243d65e3bcf0c4423a3
andry <andry46lviv@gmail.com>
2022-01-22 23:05:07 +0200

    HW_2 - implement POJO classes

---
+++ src/main/java/com/epam/spring/homework1/beans/BeanA.java
@@ -0,0 +1,14 @@
+package com.epam.spring.homework1.beans;
+
+public class BeanA {
+    String name;
+    int value;
+
+    @Override
+    public String toString() {
+        return "BeanA{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/com/epam/spring/homework1/beans/BeanF.java
@@ -0,0 +1,14 @@
+package com.epam.spring.homework1.beans;
+
+public class BeanF {
+    String name;
+    int value;
+
+    @Override
+    public String toString() {
+        return "BeanA{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
