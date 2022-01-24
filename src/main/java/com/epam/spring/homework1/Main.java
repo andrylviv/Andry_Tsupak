40835dfe782cc7994783ac7dfdff5c85a3730b86
andry <andry46lviv@gmail.com>
2022-01-24 23:44:24 +0200

    HW_2 - add bean config showing and improve code style

--- src/main/java/com/epam/spring/homework1/Main.java
+++ src/main/java/com/epam/spring/homework1/Main.java
@@ -5,7 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-
     public static void main(String[] args){
         ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
         for (String beanDefinitionName:context.getBeanDefinitionNames()) {
